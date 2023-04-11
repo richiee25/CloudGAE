8d561a0bfa3ee64d52d915ff01891f5f599b1f35
richiee25 <129824663+richiee25@users.noreply.github.com>
2023-04-11 15:22:10 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
 
-echo "hello kakak ahyar ganteng kan ?? ";
+echo "Nama    : Richie Mulyo Liauren";
+echo "<br>";
+echo "Nim     : 52020014";
+echo "<br>";
+echo "Jurusan : Teknik Informatika";
